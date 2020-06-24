fd7f531417193fb67e1118e043f929db4bc4525d
J. Zhang <jz2916@columbia.edu>
2020-06-24 15:19:11 -0400

    Next version. Almost done with homepage.

--- src/views/Landing/Landing.jsx
+++ src/views/Landing/Landing.jsx
@@ -1,26 +1,55 @@
 import React from 'react';
 import {Divider, Layout} from 'antd';
 
-import "../../styles.css"
+import "../../styles/styles.css"
 
 import FAQ from "./Sections/FAQ";
-import Samples from "./Sections/Samples";
-import Carousel from "./Sections/Carousel";
+import Welcome from "./Sections/Welcome";
 import HowItWorks from "./Sections/HowItWorks";
+import Create from "./Sections/Create"
+import Register from "./Sections/Register"
+import Resources from "./Sections/Resources"
+
 import PageWrapper from "../../PageWrapper"
+function LandingDivider(){
+    return( <Divider style={{
+        minWidth: "1em",
+        marginLeft: "10%",
+        width: "80%",
+        height: "5px",
+    }}/> )
+}
 
 class Landing extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            createRef: React.createRef(),
+            registerRef: React.createRef(),
+            resourcesRef: React.createRef(),
+        }
+    }
+
     render() {
         return (
             <PageWrapper content={
-                <Layout.Content style={{background: "white"}}>
-                    <Carousel />
+                <div style={{backgroundColor: "white"}}>
+                    <Welcome 
+                        createScroll={() => this.state.createRef.current.scrollIntoView({behavior: "smooth"})}
+                        registerScroll={() => this.state.registerRef.current.scrollIntoView({behavior: "smooth"})}
+                        resourcesScroll={() => this.state.resourcesRef.current.scrollIntoView({behavior: "smooth"})}
+                    />
+                    <LandingDivider />
                     <HowItWorks />
-                    <Divider style={{minWidth: "1em", marginLeft: "10%", width: "80%", height: "1.5px"}}/>
-                    <Samples />
-                    <Divider style={{minWidth: "1em", marginLeft: "10%", width: "80%", height: "1.5px"}}/>
+                    <LandingDivider />
+                    <div ref={this.state.createRef}> <Create /> </div>
+                    <LandingDivider />
+                    <div ref={this.state.registerRef}> <Register /> </div>
+                    <LandingDivider />
+                    <div ref={this.state.resourcesRef}> <Resources /> </div>
+                    <LandingDivider />
                     <FAQ />
-                </Layout.Content>
+                </div>
             }/>
         );
     }
